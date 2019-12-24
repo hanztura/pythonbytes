bb319a466ca9ad9d25b47ac7ec3e32c38c4bc2af
hanz <hctura.official@gmail.com>
2019-12-25 01:04:53 +0800

    production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,3 +4,16 @@ DEBUG = False
 
 RECAPTCHA_PUBLIC_KEY = os.environ.setdefault('PYTHONBYTES_RECAPTCHA_PUBLIC_KEY', '')
 RECAPTCHA_PRIVATE_KEY = os.environ.setdefault('PYTHONBYTES_RECAPTCHA_PRIVATE_KEY', '')
+
+ALLOWED_HOSTS = ['thepythonbytes.xofytech.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.set(PYTHONBYTES_DATABASE_NAME, ''),
+        'USER': os.environ.set(PYTHONBYTES_DATABASE_USER, ''),
+        'PASSWORD': os.environ.set(PYTHONBYTES_DATABASE_PASSWORD, ''),
+        'HOST': os.environ.set(PYTHONBYTES_DATABASE_HOST, ''),
+        'PORT': os.environ.set(PYTHONBYTES_DATABASE_PORT, ''),
+    }
+}
